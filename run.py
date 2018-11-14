2ab97add8c90504e570d24acf345a04289f5e8cd
Muddytm <hawkins.caleb93@gmail.com>
2018-11-13 23:08:21 -0800

    Added + functionality

--- run.py
+++ run.py
@@ -28,61 +28,32 @@ async def roll(ctx, *stuff):
         await client.say("(You might want to specify what you're rolling - examples: `!roll d20`, `!roll 2d6`, `!roll 1000d2`)")
         return
     else:
-        text = stuff[0].lower()
+        stuff = "".join(stuff)
+        stuff = stuff.split("+")
 
-        if text.startswith("d"):
-            text = text.replace("d", "")
+        total = 0
+        rolls = []
+        for token in stuff:
+            code, roll = get_roll(token)
+            roll = str(roll)
 
-            try:
-                mod = 0
-                if "+" in text:
-                    mod = int(text.split("+")[1])
-                    text = text.split("+")[0]
-
-                dice = int(text)
-            except ValueError:
-                await client.say("Invalid input!")
+            if code == 0:
+                await client.say(roll)
                 return
 
-            if dice < 1:
-                await client.say("Rolling this dice would destroy the universe. The...*authorities* have been alerted.")
-                return
+            #print (roll)
+            total += int(roll.split()[-1])
+            rolls.append(roll)
 
-            roll = random.randint(1, dice)
-            if mod == 0:
-                await client.say("{} rolled **{}**!".format(ctx.message.author.mention, str(roll)))
+        if len(rolls) > 1:
+            await client.say("{} rolled **{}**! ({})".format(ctx.message.author.mention, str(total), ", ".join(rolls)))
+        else:
+            if len(rolls[0].split()) > 1:
+                thing = rolls[0].split("=")[0]
+                thing = "".join(thing).replace(" ", "").split("+")
+                await client.say("{} rolled **{}**! ({})".format(ctx.message.author.mention, str(total), ", ".join(thing)))
             else:
-                await client.say("{} rolled **{}** ({} + {})!".format(ctx.message.author.mention, str(roll + mod), str(roll), str(mod)))
-        elif "d" in text:
-            tokens = text.split("d")
-
-            try:
-                amount = int(tokens[0])
-                dice = int(tokens[1])
-            except ValueError:
-                await client.say("Invalid input!")
-                return
-
-            if dice < 1:
-                await client.say("Rolling this dice would destroy the universe. The...*authorities* have been alerted.")
-                return
-
-            if amount < 1:
-                await client.say("*[stares at dice forlornly]*")
-                return
-
-            if amount > 100:
-                await client.say("Don't make me roll that many dice >:(")
-                return
-
-            rolls = []
-            total = 0
-            for i in range(amount):
-                roll = random.randint(1, dice)
-                total += roll
-                rolls.append(str(roll))
-
-            await client.say("{} rolled a total of **{}**! ({})".format(ctx.message.author.mention, str(total), " + ".join(rolls)))
+                await client.say("{} rolled **{}**!".format(ctx.message.author.mention, str(total)))
 
 
 @client.command(pass_context=True)
@@ -91,5 +62,53 @@ async def scram(ctx, stuff=""):
     """Get outta here, bot!"""
     await client.say("I'm outta here, gramps! *[skates away on heelies]*")
     await client.logout()
+
+
+def get_roll(text):
+    """Return int of the query passed in."""
+    if text.startswith("d"):
+        text = text.replace("d", "")
+
+        try:
+            dice = int(text)
+        except ValueError:
+            return 0, "Invalid input!"
+
+        if dice < 1:
+            return 0, "Rolling this dice would destroy the universe. The...*authorities* have been alerted."
+
+        return 1, random.randint(1, dice)
+    elif "d" in text:
+        tokens = text.split("d")
+
+        try:
+            amount = int(tokens[0])
+            dice = int(tokens[1])
+        except ValueError:
+            return 0, "Invalid input!"
+
+        if dice < 1:
+            return 0, "Rolling this dice would destroy the universe. The...*authorities* have been alerted."
+
+        if amount < 1:
+            return 0, "*[stares at dice forlornly]*"
+
+        if amount > 100:
+            return 0, "Don't make me roll that many dice >:("
+
+        rolls = []
+        total = 0
+        for i in range(amount):
+            roll = random.randint(1, dice)
+            total += roll
+            rolls.append(str(roll))
+
+        return 1, "{} = {}".format(" + ".join(rolls), str(total))
+    else:
+        try:
+            return 1, int(text)
+        except ValueError:
+            return 0, "Invalid input!"
+
 
 client.run(TOKEN)
